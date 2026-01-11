5ffaeba0e1d897075ba1a83edf09295299ba7ca2
JacieZYT <66063199+Jaciezyt@users.noreply.github.com>
2026-01-12 04:50:21 +0800

    chore: upgrade deps & update configs

--- content.config.ts
+++ content.config.ts
@@ -1,25 +1,27 @@
-import { defineCollection, defineContentConfig, z } from "@nuxt/content";
+import { defineCollection, defineContentConfig } from '@nuxt/content'
+import { z } from 'zod'
 
 export default defineContentConfig({
   collections: {
     blog: defineCollection({
-      type: "page",
-      source: "blog/*.md",
+      type: 'page',
+      source: 'blog/*.md',
+      // Define custom schema for docs collection
       schema: z.object({
         slug: z.string(),
         title: z.string(),
         description: z.string().optional(),
-        cover: z.string().optional(),
         author: z.object({
           name: z.string(),
-          avatar: z.string().optional(),
+
         }),
-        tags: z.array(z.string()),
         category: z.string(),
-        pinned: z.number().optional().default(0),
+        tags: z.array(z.string()),
+        cover: z.string().optional(),
+        pinned: z.number().optional(),
         created: z.date(),
-        updated: z.date(),
-      }),
-    }),
-  },
-});
+        updated: z.date().optional(),
+      })
+    })
+  }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,69 +1,39 @@
-import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
   modules: [
-    "@nuxt/content",
-    "@nuxt/eslint",
-    "@nuxt/image",
-    "@nuxt/ui",
-    "@pinia/nuxt",
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@pinia/nuxt',
   ],
-  ui: {
-    theme: {
-      colors: ["primary", "neutral"],
-    },
-  },
-  icon: {
-    customCollections: [
-      {
-        prefix: "my",
-        dir: "./app/assets/icon/",
-      },
-    ],
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  content: {
-    build: {
-      markdown: {
-        remarkPlugins: {
-          "remark-math": {},
-        },
-        rehypePlugins: { "rehype-katex": {} },
 
-        highlight: {
-          theme: {
-            default: "one-light",
-            dark: "one-dark-pro",
-            sepia: "monokai",
-          },
-          langs: [
-            "json",
-            "js",
-            "ts",
-            "html",
-            "css",
-            "vue",
-            "shell",
-            "mdc",
-            "md",
-            "yaml",
-            "c",
-            "cpp",
-            "java",
-            "kotlin",
-            "cmake",
-            "go",
-            "lua",
-            "python",
-            "bash",
-          ],
-        },
-      },
-    },
+  devtools: {
+    enabled: true
   },
-});
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+
+  icon: {
+    customCollections: [{
+      prefix: 'my',
+      dir: './app/assets/icon'
+    }]
+  }
+})
