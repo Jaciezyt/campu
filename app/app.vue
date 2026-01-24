6c9f1fb86f8a756f30dfa708e4dcf8be390ab4a2
JacieZYT <66063199+Jaciezyt@users.noreply.github.com>
2026-01-25 00:30:28 +0800

    feat: provide more seo meta

--- app/app.vue
+++ app/app.vue
@@ -1,21 +1,40 @@
-<template>
-  <Head>
-    <meta name="description" :content="config.siteMeta.description" />
-    <meta name="keywords" :content="config.siteMeta.keywords.join(',')" />
-  </Head>
+<script setup lang="ts">
+const { siteMeta } = useAppConfig();
 
+const route = useRoute();
+const title = route.meta.title ? `${route.meta.title} | ${siteMeta.name}` : siteMeta.name;
+const description = route.meta.description ? String(route.meta.description) : siteMeta.description;
+const ogImage = route.meta.ogImage ? route.meta.ogImage : siteMeta.defaultOgImage;
+
+useHead({
+  title: title,
+  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+
+route.meta.title;
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: ogImage,
+  twitterCard: "summary_large_image",
+  twitterTitle: title,
+  twitterDescription: description,
+  twitterSite: siteMeta.twitterSite,
+  twitterImage: ogImage,
+});
+</script>
+
+<template>
   <UApp>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </UApp>
 </template>
-
-<script setup lang="ts">
-const config = useAppConfig();
-useHead({
-  titleTemplate: (titleChunk: any) => {
-    return titleChunk ? `${titleChunk} | ${config.siteMeta.name}` : config.siteMeta.name;
-  },
-});
-</script>
