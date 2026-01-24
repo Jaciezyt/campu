720f9bbd53500d384f2941b4c2a5b68ff9ce740a
JacieZYT <66063199+Jaciezyt@users.noreply.github.com>
2026-01-25 00:38:57 +0800

    refactor: use fetch instead of axios

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 let lastUpdateHitokoto = 0;
 const curHitokoto = ref({
   hitokoto: "合抱之木，生于毫末；九层之台，起于累土；千里之行，始于足下。",
@@ -11,10 +9,9 @@ export function updateHitokoto(): Ref<{ hitokoto: string; from: string }> {
     return curHitokoto;
   }
   lastUpdateHitokoto = Date.now();
-  axios
-    .get("https://v1.hitokoto.cn?c=a&c=b&c=c&c=d&c=h&c=i&c=k")
-    .then((response) => {
-      curHitokoto.value = response.data;
+  fetch("https://v1.hitokoto.cn?c=a&c=b&c=c&c=d&c=h&c=i&c=k")
+    .then(async (response) => {
+      curHitokoto.value = await response.json();
     })
     .catch((error) => {
       console.error(error);
